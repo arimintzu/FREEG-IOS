85f611582f26190108e73d3e81861cf95a561089
Dewa732 <dewa732@Dewa732s-Mac.local>
2018-11-19 19:54:53 -0800

    Initial Commit

---
+++ UAS1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UAS1
+//
+//  Created by Dewa732 on 19/11/18.
+//  Copyright © 2018 Dewa732. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
